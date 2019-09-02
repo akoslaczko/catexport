272b4a3d1b71f1d4694b2d32f4972f259989f141
akoslaczko <akoslaczko@github.com>
2019-09-02 16:12:27 +0200

    Created Cat Stock Menu

---
+++ models/cat.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class CatCat(models.Model):
+    _name = 'cat.cat'
+
+    breed = fields.Char(string='Breed', required=True)
+    stock = fields.Integer(string='Stock')
